e2be1b9b32feeda4bf0e8a953e4a9e1bca4b7c5c
Richard Ho <Richard.Ho@moblab.com>
2015-07-09 00:45:17 -0700

    Changing port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var Restaurants = require('./app/models/preferences/restaurants');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8081;
+var port = process.env.PORT || 777;
 
 var router = express.Router();
 router.route('/slack')
